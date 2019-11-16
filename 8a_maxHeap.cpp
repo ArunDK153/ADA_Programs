37ffc41a2c642dc30aee950bc462e6fd376ee11f
ArunDK153 <54391160+ArunDK153@users.noreply.github.com>
2019-11-16 13:03:30 +0530

    Add files via upload

---
+++ 8a_maxHeap.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+bool isHeap(int arr[],  int n)
+{
+    for (int i=0; i<=(n-2)/2; i++)
+    {
+        if (arr[2*i +1] > arr[i])
+                return false;
+        if (2*i+2 < n && arr[2*i+2] > arr[i])
+                return false;
+    }
+    return true;
+}
+
+int main()
+{
+    int arr[50],n;
+  	std::cout<<"Enter number of elements: ";
+	std::cin>>n;
+	std::cout<<"Enter elements: ";
+	for(int i =0;i<n;i++)
+	{
+		std::cin>>arr[i];
+	}
+	std::cout<<"Is it binary max heap? ";
+    isHeap(arr, n)? std::cout<<"Yes" : std::cout<<"No";
+    return 0;
+}
